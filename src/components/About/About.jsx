3ea696de2e57960a678cdae0eda5c047cd83d324
AndileMnukwa <mnukwa1996@gmail.com>
2024-07-14 00:28:37 +0200

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './About.css'
 // import grad from '../../assets/grad.jpeg'
-import profile_img from '../../assets/profile_img.png'
+import profile_img from '../../assets/profile-img.png'
 
 
 const About = () => {
@@ -16,31 +16,35 @@ const About = () => {
                 <img src={profile_img} alt="" />
             </div>
             <div className="about-right">
-                <div className="about-para">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ducimus quo est facilis aliquam autem odio aperiam vitae, doloremque libero, quidem distinctio molestias rerum earum labore beatae? Voluptates, possimus optio.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eos sequi, perferendis laudantium consectetur dolorem minima quos suscipit ipsam atque dolore labore vel beatae voluptatem ut ducimus odit illum expedita.</p>
+                <div className="about-right">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.possimus optio.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
                 </div>
-                <div className="about-skills"><p>HTML & CSS</p><hr style={{width:"50%"}} /></div>
-                <div className="about-skills"><p>React JS</p><hr style={{width:"50%"}} /></div>
-                <div className="about-skills"><p>Python</p><hr style={{width:"50%"}} /></div>
-                <div className="about-skills"><p>JavaScript</p><hr style={{width:"50%"}} /></div>
-                <div className="about-skills"><p>Java</p><hr style={{width:"50%"}} /></div>
+                <div className="about-skills">
+                <div className="about-skill"><p>HTML & CSS</p><hr style={{width:"70%"}} /></div>
+                <div className="about-skill"><p>React JS</p><hr style={{width:"60%"}} /></div>
+                <div className="about-skill"><p>Python</p><hr style={{width:"60%"}} /></div>
+                <div className="about-skill"><p>JavaScript</p><hr style={{width:"55%"}} /></div>
+                <div className="about-skill"><p>Java</p><hr style={{width:"70%"}} /></div>
             </div>
         </div>
+        </div>
         <div className="about-achievements">
+        <div className="about-achievement">
             <h1>10+</h1>
             <p>Years Of Experience</p>
         </div>
-        <hr/>
+        <hr />
         <div className="about-achievement">
             <h1>90+</h1>
             <p>Project Completed</p>
         </div>
-        <hr/>
+        <hr />
         <div className="about-achievement">
             <h1>15+</h1>
             <p>Happy client</p>
         </div>
+    </div>
     </div>
   )
 }
